c533456fbbc3fd4cf2c99d84c337005fd53b3574
Anthony <anthony.leung.canada@gmail.com>
2018-11-25 19:51:21 -0500

    Added a general class to create a vertically stacked window of labels and textfields

---
+++ DesignPrinciples/src/main/java/com/app/gui/menu/frames/VerticalFieldsFrame.java
@@ -0,0 +1,58 @@
+package com.app.gui.menu.frames;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import com.app.bases.FieldsBase;
+import com.app.helpers.JGuiHelper;
+import com.app.product.DbCol;
+
+/**
+ * Creates a fields frame with the labels on the left side
+ * and the text fields on the right side
+ * 
+ * @author antho
+ *
+ */
+@SuppressWarnings("serial")
+public class VerticalFieldsFrame extends FieldsBase {
+
+	private String tableName;
+	private DbCol[] columns;
+	private HashMap<DbCol, JTextField> dataFields;
+	
+	public VerticalFieldsFrame(String title, DbCol[] productClass) {
+		super(title);
+		this.tableName = title;
+		this.columns = productClass;
+		this.dataFields = new HashMap<DbCol, JTextField>();
+		super.setBody(getFieldsPanel());
+	}
+
+	@Override
+	protected JPanel getFieldsPanel() {
+		List<String> labels = new ArrayList<String>();
+		for (DbCol col : columns) {
+			labels.add(col.getDisplayName());
+		}
+		
+		List<JTextField> textFields = JGuiHelper.createTextFields(labels.size());
+		for (int i = 0; i < labels.size(); i ++) {
+			dataFields.put(columns[i], textFields.get(i));
+		}
+		
+		JPanel p = JGuiHelper.createLabelPanel(labels, textFields);
+		return p;
+	}
+
+	@Override
+	protected boolean submitAction() {
+		// TODO Insert all the fields from the hashmap into the database
+		tableName.length();
+		return false;
+	}
+}
--- DesignPrinciples/src/main/java/com/app/helpers/JGuiHelper.java
+++ DesignPrinciples/src/main/java/com/app/helpers/JGuiHelper.java
@@ -1,8 +1,17 @@
 package com.app.helpers;
 
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class JGuiHelper {
 
@@ -12,4 +21,74 @@ public class JGuiHelper {
 		btn.setActionCommand(actionCommand);
 		return btn;
 	}
+
+	public static List<JTextField> createTextFields(int number) {
+		List<JTextField> fields = new ArrayList<JTextField>();
+
+		for (int i = 0; i < number; i++) {
+			fields.add(new JTextField(20));
+		}
+
+		return fields;
+	}
+
+	/**
+	 * Creates a (n by 2) grid layout where the left side is the labels and the
+	 * right side is the fields. N is the number of fields. The number of labels
+	 * must equal to the number of textfields.
+	 * 
+	 * @param labelText
+	 * @param textfields
+	 * @return
+	 */
+	public static JPanel createLabelPanel(List<String> labels, List<JTextField> textFields) {
+		int size;
+		if (labels.size() == textFields.size()) {
+			size = labels.size();
+		} else {
+			return null;
+		}
+
+		JPanel p = new JPanel();
+		p.setLayout(new GridLayout(size, 2));
+
+		for (int i = 0; i < size; i++) {
+			p.add(new JLabel(labels.get(i)));
+			p.add(textFields.get(i));
+		}
+
+		return p;
+	}
+
+	
+	// These under not used atm
+	/**
+	 * Creates a panel with the preset layout for BoxLayout
+	 * 
+	 * Can be one of: BoxLayout.X_AXIS, BoxLayout.Y_AXIS, BoxLayout.LINE_AXIS
+	 * BoxLayout.PAGE_AXIS
+	 * 
+	 * @param layout_axis
+	 * @return
+	 */
+
+	public static JPanel createPanelBox(int layout_axis) {
+		JPanel p = new JPanel();
+		BoxLayout pLayout = new BoxLayout(p, layout_axis);
+		p.setLayout(pLayout);
+		p.setAlignmentX(Component.LEFT_ALIGNMENT);
+		return p;
+	}
+
+	/**
+	 * Creates a panel with a preset layout for FLowLayout aligned to the left
+	 * 
+	 * @return
+	 */
+	public static JPanel createPanelFlow() {
+		JPanel p = new JPanel(new FlowLayout());
+		p.setAlignmentX(Component.LEFT_ALIGNMENT);
+		return p;
+	}
+
 }
